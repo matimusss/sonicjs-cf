83ecdabba0d0fe30a4f4a233ccda0f61c47f45f7
matimusa@gmail.com <ejemplo@email.com>
2024-05-09 11:14:12 -0300

    ASD

--- src/cms/admin/theme.tsx
+++ src/cms/admin/theme.tsx
@@ -273,7 +273,7 @@ export const Layout: FC<{
                     </a>
                   </li>
                   <li class='nav-item'>
-                    <a class='nav-link' href='/public/grapes/index.html'>
+                    <a class='nav-link' href='/'>
                       Editor de horarios
                     </a>
                   </li>
