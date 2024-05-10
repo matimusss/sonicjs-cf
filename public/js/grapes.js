dfa24fe60c3b2dc20e622cb1121fd7b854907f3c
matimusa@gmail.com <ejemplo@email.com>
2024-05-10 20:37:53 -0300

    ASD

--- public/js/grapes.js
+++ public/js/grapes.js
@@ -1,13 +1,3 @@
-const loadScript = () => {
-  setTimeout(() => {
-    var newScript = document.createElement('script');
-    newScript.type = 'text/javascript';
-    newScript.src = '/public/js/grapes.js';
-    document.body.appendChild(newScript);
-  }, 1000); // Espera un segundo (1000 milisegundos) antes de cargar el script
-};
-
-
 
 
 
--- src/cms/admin/pages/content.tsx
+++ src/cms/admin/pages/content.tsx
@@ -585,37 +585,12 @@ export async function prueba() {
 
   return (
     <Layout>
-    <div id="gjs" style="height:300px ; width: 1800px">
+    <div id="gjs" style="height:100vh" class="row">
     </div>
     </Layout>
   );
 };
 
-
-
-
-export async function prueba2() {
-  const onEditor = (editor: Editor) => {
-    console.log('Editor loaded', { editor });
-  };
-
-  return (
-    <GjsEditor
-      // Pass the core GrapesJS library to the wrapper (required).
-      // You can also pass the CDN url (eg. "https://unpkg.com/grapesjs")
-      grapesjs={grapesjs}
-      // Load the GrapesJS CSS file asynchronously from URL.
-      // This is an optional prop, you can always import the CSS directly in your JS if you wish.
-      grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
-      // GrapesJS init options
-      options={{
-        height: '100vh',
-        storageManager: false,
-      }}
-      onEditor={onEditor}
-    />
-  );
-}
 
 
 
