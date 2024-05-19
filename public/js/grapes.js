d76bc21c2d94e9f9e7b17f9f96d8ed4d378ae8f7
matimusa@gmail.com <ejemplo@email.com>
2024-05-18 22:24:24 -0300

    asd

--- public/js/grapes.js
+++ public/js/grapes.js
@@ -430,6 +430,7 @@ for (var i = 0; i < titles.length; i++) {
 
 // Store and load events
 editor.on('storage:load', function(e) { console.log('Loaded ', e) });
+
 editor.on('storage:store', function(e) { console.log('Stored ', e  ) });
 
 editor.on('storage:store', function(editor) { console.log(editor.getHtml()) });
