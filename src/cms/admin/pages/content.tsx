f8b25818d0809ac7dd02a68bae2dd4b9dbef9532
matimusa@gmail.com <ejemplo@email.com>
2024-05-20 08:00:23 -0300

    ASD

--- src/cms/admin/pages/content.tsx
+++ src/cms/admin/pages/content.tsx
@@ -607,16 +607,14 @@ export async function prueba(ctx) {
 
 
 export async function pruebaReact(ctx) {
+  const onEditor = (editor: Editor) => {
+    console.log('Editor loaded', { editor });
+  };
   return (  
     <Layout>  
   <GjsEditor
-      // Pass the core GrapesJS library to the wrapper (required).
-      // You can also pass the CDN url (eg. "https://unpkg.com/grapesjs")
       grapesjs={grapesjs}
-      // Load the GrapesJS CSS file asynchronously from URL.
-      // This is an optional prop, you can always import the CSS directly in your JS if you wish.
       grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
-      // GrapesJS init options
       options={{
         height: '100vh',
         storageManager: false,
