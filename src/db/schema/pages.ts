dd4f526aee54be2375d42fd8908f4eb7d9fd6c6b
matimusa@gmail.com <ejemplo@email.com>
2024-05-23 03:13:53 -0300

    asd

--- src/db/schema/pages.ts
+++ src/db/schema/pages.ts
@@ -8,7 +8,7 @@ export const route = 'pages';
 
 // Definición de la estructura de la tabla
 export const definition = {
-  ID: integer('ID').primaryKey(), // Se define como clave primaria e INTEGER
+  ID: integer('ID'), // Se define como clave primaria e INTEGER
   name: text('name'), // Campo de texto para el nombre de la página
   slug: text('slug'), // Campo de texto para el slug de la página
   html_code: text('html_code'), // Campo de texto para el código HTML de la página
