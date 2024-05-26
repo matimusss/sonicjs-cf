31038e58777947f3c869366b76e92b2c989a0df4
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 03:15:06 -0300

    ad

--- public/js/sluger.js
+++ public/js/sluger.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   setTimeout(function() {
     // Espera 1 segundo antes de ejecutar el código
-    const formioFields = document.querySelectorAll('#formio input[name="[data]slug"]');
+    const formioFields = document.querySelectorAll(' input[name="[data]slug"]');
     formioFields.forEach(function(field) {
       // Ejecutar acciones X para cada campo [data]slug encontrado
       // Por ejemplo, cambiar el valor del campo o realizar alguna acción específica
