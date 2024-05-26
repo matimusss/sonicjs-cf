a66133f5c44f69f17bdb2e025951c18cb7669dcf
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 03:07:03 -0300

    ad

--- public/js/sluger.js
+++ public/js/sluger.js
@@ -1,10 +1,5 @@
-setTimeout(function() {
-  
   window.onload = function() {
     setTimeout(function() {
       alert("La página se ha cargado completamente");
     }, 1000); // Espera 1 segundo antes de mostrar la alerta
   };
-
-}, 3000);
-//}
