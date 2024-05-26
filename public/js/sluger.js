1849fe8e9050564ef68ca07c504dbdeaed608b3a
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 05:49:41 -0300

    asd

--- public/js/sluger.js
+++ public/js/sluger.js
@@ -1,14 +1,24 @@
 window.onload = function() {
   setTimeout(function() {
+  var Slugify = require('slugifyjs').fromLocale('en');
 
     alert("se cargo");
     // Espera 1 segundo antes de ejecutar el código
 
-    const formioFields = document.querySelectorAll('[id*="slug"]');
-    formioFields.forEach(function(field) {
-      field.style.backgroundColor = 'yellow';
-      alert("se detecto un slug");
-    });
+    const slug = document.querySelectorAll('[id*="slug"]');
+    const  nameOrTitle = document.querySelectorAll('[id*="name"], [id*="title"]');
+
+
+
+    slug.forEach(function(field) {
+      field.style.backgroundColor = 'yellow'; 
+      const titulo = nameOrTitle.name;
+      alert("se cargo" + titulo  + ".");
+   
+});
+
+
+
   }, 1600); // Espera 1 segundo antes de ejecutar el código
 
 
