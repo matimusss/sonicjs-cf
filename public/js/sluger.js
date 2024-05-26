131cf03f0c9a0655aa19744d5299b31b1576ce92
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 03:11:55 -0300

    ad

--- public/js/sluger.js
+++ public/js/sluger.js
@@ -1,5 +1,14 @@
-  window.onload = function() {
-    setTimeout(function() {
-      alert("La página se ha cargado completamente");
-    }, 1000); // Espera 1 segundo antes de mostrar la alerta
-  };
+window.onload = function() {
+  setTimeout(function() {
+    // Espera 1 segundo antes de ejecutar el código
+    const formioFields = document.querySelectorAll('#formio input[name="[data]slug"]');
+    formioFields.forEach(function(field) {
+      // Ejecutar acciones X para cada campo [data]slug encontrado
+      // Por ejemplo, cambiar el valor del campo o realizar alguna acción específica
+      // Aquí puedes agregar tu lógica personalizada
+      field.value = 'SLUG';
+      console.log('Valor del campo [data]slug cambiado a "SLUG"');
+      alert("HAY UN CAMPO SLUG");
+    });
+  }, 1000); // Espera 1 segundo antes de ejecutar el código
+};
