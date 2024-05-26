c374a0b707e5176c354f205c6581e486a7cc9634
matimusa@gmail.com <ejemplo@email.com>
2024-05-26 03:17:38 -0300

    ad

--- public/js/sluger.js
+++ public/js/sluger.js
@@ -1,14 +1,11 @@
 window.onload = function() {
   setTimeout(function() {
+
+    alert("se cargo");
     // Espera 1 segundo antes de ejecutar el código
     const formioFields = document.querySelectorAll(' input[name="[data]slug"]');
     formioFields.forEach(function(field) {
-      // Ejecutar acciones X para cada campo [data]slug encontrado
-      // Por ejemplo, cambiar el valor del campo o realizar alguna acción específica
-      // Aquí puedes agregar tu lógica personalizada
-      field.value = 'SLUG';
-      console.log('Valor del campo [data]slug cambiado a "SLUG"');
-      alert("HAY UN CAMPO SLUG");
+      alert("se detecto un slug");
     });
   }, 1000); // Espera 1 segundo antes de ejecutar el código
 };
