09b35b5605816ece52d376565cd0a2cbe896be4f
matimusa@gmail.com <ejemplo@email.com>
2024-05-30 16:32:20 -0300

    asd

--- public/js/sluger.js
+++ public/js/sluger.js
@@ -1,25 +1,25 @@
-window.onload = function() {
-  setTimeout(function() {
+//window.onload = function() {
+//  setTimeout(function() {
 
 
-    alert("se cargo");
+//    alert("se cargo");
     // Espera 1 segundo antes de ejecutar el código
 
-    const slug = document.querySelectorAll('[id*="slug"]');
-    const  nameOrTitle = document.querySelectorAll('[id*="name"], [id*="title"]');
+  //  const slug = document.querySelectorAll('[id*="slug"]');
+    //const  nameOrTitle = document.querySelectorAll('[id*="name"], [id*="title"]');
 
 
 
-    slug.forEach(function(field) {
-      field.style.backgroundColor = 'yellow'; 
-      const titulo = nameOrTitle[0].getAttribute("value"); 
-      alert("se cargo" + titulo  + ".");
+   // slug.forEach(function(field) {
+     // field.style.backgroundColor = 'yellow'; 
+      //const titulo = nameOrTitle[0].getAttribute("value"); 
+      //alert("se cargo" + titulo  + ".");
    
-});
+//});
 
 
 
-  }, 1600); // Espera 1 segundo antes de ejecutar el código
+  //}, 1600); // Espera 1 segundo antes de ejecutar el código
 
 
-};
+//};
