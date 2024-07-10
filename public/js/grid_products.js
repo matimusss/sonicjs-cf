6d15e3bafba767cc103e67046da95e6e671ea34f
matimusa@gmail.com <ejemplo@email.com>
2024-07-10 15:26:53 -0300

    asdasd

--- public/js/grid_products.js
+++ public/js/grid_products.js
@@ -1,11 +1,10 @@
 const url2 = window.location.href;
-const authMode = url2.includes('/auth/');
 const gridWrapper2 = document.getElementById('grid-products');
 
 
 if (gridWrapper2) {
     console.log("MOUNTEDDDDDD");
-  const dataGrid = new gridjs.Grid({
+  const dataGrid2 = new gridjs.Grid({
     data: [
         { name: 'John', email: 'john@example.com', phoneNumber: '(353) 01 222 3333' },
         { name: 'Mark', email: 'mark@gmail.com', phoneNumber: '(01) 22 888 4444' },
