5baa45047abb3042f638b5bfc5523153a10f13ce
matimusa@gmail.com <ejemplo@email.com>
2024-07-10 16:41:35 -0300

    asdasd

--- public/js/grid.js
+++ public/js/grid.js
@@ -3,16 +3,26 @@ const authMode = url.includes('/auth/');
 const gridWrapper = document.getElementById('grid');
 if (gridWrapper) {
   const dataGrid = new gridjs.Grid({
-
-
-    columns: ["Name", "Email", "Phone Number"],
-    data: [
-      ["John", "john@example.com", "(353) 01 222 3333"],
-      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-      ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-      ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-      ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+    columns: [
+      {
+        name: 'Record',
+        formatter: (displayValue) => gridjs.html(`${displayValue}`)
+      },
+      {
+        name: 'Updated',
+        formatter: (dt) =>
+          gridjs.html(`<time class="timeSince" datetime="${dt}">${dt}</time>`)
+      },
+      {
+        name: 'API',
+        formatter: (editPath) => gridjs.html(`${editPath}`)
+      },
+      {
+        name: 'Actions',
+        formatter: (actionButtons) => gridjs.html(`${actionButtons}`)
+      }
     ],
+
     pagination: {
       limit: 10,
       server: {
@@ -21,48 +31,6 @@ if (gridWrapper) {
       }
     },
 
-    server: {
-      url: authMode
-        ? `/admin/api/auth/${getTable()}`
-        : `/admin/api/${getTable()}`,
-      data: (opts) => {
-        return new Promise((resolve, reject) => {
-          // let's implement our own HTTP client
-          const xhttp = new XMLHttpRequest();
-          const start = Date.now();
-
-          xhttp.onreadystatechange = function () {
-            if (this.readyState === 4) {
-              if (this.status === 200) {
-                const resp = JSON.parse(this.response);
-                $('#executionTime').show();
-                $('#executionTime span.serverTime').text(resp.executionTime);
-                $('#executionTime span.source').text(resp.source);
-                // make sure the output conforms to StorageResponse format:
-                // https://github.com/grid-js/gridjs/blob/master/src/storage/storage.ts#L21-L24
-
-                const end = Date.now();
-                const clientExecutionTime = end - start;
-                $('#executionTime span.clientTime').text(clientExecutionTime);
-                resolve({
-                  data: resp.data.map((record) => [
-                    record.displayValue,
-                    record.updatedOn,
-                    record.apiLink,
-                    record.actionButtons
-                  ]),
-                  total: resp.total
-                });
-              } else {
-                reject();
-              }
-            }
-          };
-          xhttp.open('GET', opts.url, true);
-          xhttp.send();
-        });
-      }
-    }
   }).render(gridWrapper);
 
 
