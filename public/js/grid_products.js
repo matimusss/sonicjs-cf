9976f05c9d77f5dca560c91599d403dab58cc63b
matimusa@gmail.com <ejemplo@email.com>
2024-07-10 16:56:29 -0300

    asdasd

--- public/js/grid_products.js
+++ public/js/grid_products.js
@@ -41,6 +41,33 @@ if (gridWrapperProducts) {
           const xhttp = new XMLHttpRequest();
           const start = Date.now();
 
+          xhttp.onreadystatechange = function () {
+            if (this.readyState === 4) {
+              if (this.status === 200) {
+                const resp = JSON.parse(this.response);
+                $('#executionTime').show();
+                $('#executionTime span.serverTime').text(resp.executionTime);
+                $('#executionTime span.source').text(resp.source);
+                // make sure the output conforms to StorageResponse format:
+                // https://github.com/grid-js/gridjs/blob/master/src/storage/storage.ts#L21-L24
+
+                const end = Date.now();
+                const clientExecutionTime = end - start;
+                $('#executionTime span.clientTime').text(clientExecutionTime);
+                resolve({
+                  data: resp.data.map((record) => [
+                    record.displayValue,
+                    record.updatedOn,
+                    record.apiLink,
+                    record.actionButtons
+                  ]),
+                  total: resp.total
+                });
+              } else {
+                reject();
+              }
+            }
+          };
           xhttp.open('GET', opts.url, true);
           xhttp.send();
         });
