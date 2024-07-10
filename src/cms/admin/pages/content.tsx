7f75290e6fcea03d6630eb124be530285f36ce71
matimusa@gmail.com <ejemplo@email.com>
2024-07-10 15:17:33 -0300

    asdasd

--- src/cms/admin/pages/content.tsx
+++ src/cms/admin/pages/content.tsx
@@ -732,8 +732,15 @@ export async function ProductForm(ctx) {
 
   return (
     <Layout env={ctx.env}>
-                <div id='grid-products'></div>
-                <script src='/public/js/grid_products.js'></script>
+          
+  <script src='/public/js/grid_products.js'></script>
+  <script src='https://unpkg.com/gridjs/dist/gridjs.umd.js'></script>
+<script src='/public/js/grid.js'></script>
+<script src='/public/js/grid-products.js'></script>
+<script src='/public/js/grid-in-memory-cache.js'></script>
+<script src='/public/js/grid-kv-cache.js'></script>
+  <link href='/public/css/gridjs.css' rel='stylesheet' />
+      <div id='grid-products'></div>
    <NewProduct />
     </Layout>
   );
