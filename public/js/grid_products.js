73bac4bcd5f3b6a125e41c4ab83d40313d89a567
matimusa@gmail.com <ejemplo@email.com>
2024-07-10 16:48:20 -0300

    asdasd

--- public/js/grid_products.js
+++ public/js/grid_products.js
@@ -2,16 +2,16 @@ const url = window.location.href;
 const authMode = url.includes('/auth/');
 const gridWrapperProducts = document.getElementById('grid-products');
 if (gridWrapperProducts) {
-  const dataGrid = new gridjs.Grid({
-    columns: ['Name', 'Email', 'Phone Number'],
-    data: [
-      ['John', 'john@example.com', '(353) 01 222 3333'],
-      ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
-    ]
-
- 
-  
-  }).render(gridWrapperProducts);
+    new gridjs.Grid({
+        columns: ["Name", "Email", "Phone Number"],
+        data: [
+          ["John", "john@example.com", "(353) 01 222 3333"],
+          ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+          ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+          ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+          ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+        ]
+      }).render(document.getElementById("grid-products"));
 
 
   function deleteItem(itemId) {
