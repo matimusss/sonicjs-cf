8854f1a5551ba37f89c8cf65c7215d56f4b102a1
matimusa@gmail.com <ejemplo@email.com>
2024-07-11 08:11:13 -0300

    asdasd

--- src/cms/api/api.ts
+++ src/cms/api/api.ts
@@ -81,7 +81,7 @@ tables.forEach((entry) => {
   api.get('/product-min-details', async (ctx) => {
     try {
       // Llama a la función getD1ByTableAndId para obtener los datos del producto
-      const data = await getD1byname_view(   ctx.env.D1DATA, 'product_min_details', id);
+      const data = await getD1byname_view(   ctx.env.D1DATA, 'product_min_details');
       if (data) {
         return ctx.json(data);
       } else {
--- src/cms/data/d1-data.ts
+++ src/cms/data/d1-data.ts
@@ -32,12 +32,12 @@ export async function getD1ByTableAndId_view(db, table, id) {
   }
 }
 
-export async function getD1byname_view(db, table, id) {
+export async function getD1byname_view(db, table) {
   // Define la consulta SQL con un parámetro de reemplazo
   let sql = `SELECT * FROM ${table}`;
   try {
     // Prepara y ejecuta la consulta SQL con el parámetro proporcionado
-    const { results } = await db.prepare(sql).bind(id).all();
+    const { results } = await db.prepare(sql).all();
     return results; // Devuelve los resultados de la consulta
   } catch (error) {
     console.error('Error executing SQL:', error);
