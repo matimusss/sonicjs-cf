4bec769394f76ba38fd1619377b2fd21cd573407
matimusa@gmail.com <ejemplo@email.com>
2024-07-11 06:55:46 -0300

    asdasd

--- src/cms/admin/theme.tsx
+++ src/cms/admin/theme.tsx
@@ -265,8 +265,32 @@ export const Layout: FC<{
               <div class='position-sticky pt-3 sidebar-sticky'>
                 <ul class='nav flex-column'>
 
-
-               
+                  
+                  <li class='nav-item'>
+                   
+                   
+                    <a class='nav-link' href='/admin'>
+                      API
+                    </a>
+                  </li>
+                  <h6 class='sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted'>
+                    <span>Tables</span>
+                  </h6>
+                  {tables
+                    .filter((t) => t.route !== 'users')
+                    .map((item: ApiConfig) => {
+                      return (
+                        <li class='nav-item'>
+                          <a
+                            class='nav-link'
+                            href={'/admin/tables/' + item.route}
+                          >
+                            {item.route}
+                          </a>
+                        </li>
+                      );
+                    })}
+                  <>
 
 
                     <h6 class='sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted'>
@@ -370,31 +394,7 @@ export const Layout: FC<{
                     </a>
                   </li>
   
-                  <li class='nav-item'>
-                   
-                   
-                   <a class='nav-link' href='/admin'>
-                     API
-                   </a>
-                 </li>
-                 <h6 class='sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted'>
-                   <span>Tables</span>
-                 </h6>
-                 {tables
-                   .filter((t) => t.route !== 'users')
-                   .map((item: ApiConfig) => {
-                     return (
-                       <li class='nav-item'>
-                         <a
-                           class='nav-link'
-                           href={'/admin/tables/' + item.route}
-                         >
-                           {item.route}
-                         </a>
-                       </li>
-                     );
-                   })}
-                 <>
+
             
            
 
