68efe77c6ef613f4327d7032c850da228d40149d
matimusa@gmail.com <ejemplo@email.com>
2024-07-15 03:42:04 -0300

    asd

--- public/js/form-products.js
+++ public/js/form-products.js
@@ -1,121 +1,123 @@
 
-
-
-
-
-
 (function () {
 Formio.createForm(document.getElementById('formio-attributes'), {
   components: [
     {
+      label: 'Atributos',
+      key: 'attributes_form',
       type: 'editgrid',
-      label: 'Attributes',
-      key: 'attributes',
-      defaultOpen: true,
-      removeRow: 'Cancel',
-      components: 
+      input: true,
+      templates: {
+        header: '' +
+          '<div class="row">' +
+          '  {% util.eachComponent(components, function(component) { %} ' +
+          '    <div class="col-sm-2">' +
+          '      <strong>{{ component.label }}</strong>' +
+          '    </div>' +
+          '  {% }) %}' +
+          '</div>',
+        row: '' +
+          '<div class="row">' +
+          '  {%util.eachComponent(components, function(component) { %}' +
+          '    <div class="col-sm-2">' +
+          '      {{ row[component.key] }}' +
+          '    </div>' +
+          '  {% }) %}' +
+          '  <div class="col-sm-2">' +
+          '    <div class="btn-group pull-right">' +
+          '      <div class="btn btn-default btn-sm editRow"><i class="bi bi-edit"></i></div>' +
+          '      <div class="btn btn-danger btn-sm removeRow"><i class="bi bi-trash"></i></div>' +
+          '    </div>' +
+          '  </div>' +
+          '</div>',
+        footer: ''
+      },
+      components: [
+        {
+          label: 'Atributo',
+          key: 'attribute',
+          type: 'select',
+          input: true,
+          data: {
+            values: [
+              {
+                value: 'color',
+                label: 'Color'
+              },
+              {
+                value: 'peso',
+                label: 'Peso'
+              },
+              {
+                value: 'material',
+                label: 'Material'
+              }
+            ]
+          },
+          dataSrc: 'values',
+          template: '<span>{{ item.label }}</span>'
+        },
+        {
+          label: 'Valores',
+          key: 'value',
+          type: 'select',
+          input: true,
+          data: {
+            values: [
+              {
+                value: '15kg',
+                label: '15kg'
+              },
+              {
+                value: '10kg',
+                label: '10kg'
+              },
+              {
+                value: '11kg',
+                label: '11kg'
+              }
+            ]
+          },
+          dataSrc: 'values',
+          template: '<span>{{ item.label }}</span>'
+        },
 
-      [
-        {
-          "label": "Enter your annual income (trigger)",
-          "placeholder": "EX $52,000",
-          "tooltip": "Input a value less than 45,000 to trigger the condition",
-          "mask": false,
-          "spellcheck": true,
-          "tableView": false,
-          "currency": "USD",
-          "inputFormat": "plain",
-          "truncateMultipleSpaces": false,
-          "key": "income",
-          "type": "currency",
-          "input": true,
-          "delimiter": true
-        },
-        {
-          "label": "Select your marital status (trigger)",
-          "optionsLabelPosition": "right",
-          "tooltip": "Select Single or Widowed to trigger the condition",
-          "inline": false,
-          "tableView": false,
-          "values": [
-            {
-              "label": "Single",
-              "value": "single",
-              "shortcut": ""
-            },
-            {
-              "label": "Married",
-              "value": "married",
-              "shortcut": ""
-            },
-            {
-              "label": "Separated",
-              "value": "separated",
-              "shortcut": ""
-            },
-            {
-              "label": "Widowed",
-              "value": "widowed",
-              "shortcut": ""
-            }
-          ],
-          "key": "maritalStatus",
-          "type": "radio",
-          "input": true
-        },
-        {
-          "title": "Conditional Panel",
-          "collapsible": false,
-          "key": "panel",
-          "customConditional": "show = (data.income \u003C 45000) & (data.maritalStatus == 'single' || data.maritalStatus == 'widowed');",
-          "type": "panel",
-          "label": "Panel",
-          "input": false,
-          "tableView": false,
-          "components": [
-            {
-              "label": "HTML",
-              "attrs": [
-                {
-                  "attr": "",
-                  "value": ""
-                }
-              ],
-              "content": "You are eligible for financial assistance:",
-              "refreshOnChange": false,
-              "key": "html",
-              "type": "htmlelement",
-              "input": false,
-              "tableView": false
-            },
-            {
-              "label": "Enter your email address for application information",
-              "placeholder": "Please enter a valid email address",
-              "tableView": true,
-              "key": "enterYourEmailAddressForApplicationInformation",
-              "type": "email",
-              "input": true
-            }
-          ]
-        },
-        {
-          "label": "\u003Cb\u003EReset Form\u003C/b\u003E",
-          "action": "reset",
-          "showValidations": false,
-          "theme": "secondary",
-          "block": true,
-          "disableOnInvalid": true,
-          "tableView": false,
-          "key": "submit",
-          "type": "button",
-          "input": true
-        }
       ]
-      
     }
   ]
+}).then(function(form) {
+// Provide a default submission.
+form.submission = {
+  data: {
+    attributes_form: [
+      {
+        attribute: 'Peso',
+        value: '15kg',
+      },
+      {
+        attribute: 'Color',
+        value: 'Rojo',
+      },
+    ]
+  }
+};
 });
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
