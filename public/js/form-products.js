024deb0a0a7698c62572af5dcfdb5ebb29092ce2
matimusa@gmail.com <ejemplo@email.com>
2024-07-15 12:12:53 -0300

    asd

--- public/js/form-products.js
+++ public/js/form-products.js
@@ -312,32 +312,32 @@ Formio.createForm(document.getElementById('formio-variants'), {
       tableView: false,
       displayAsTable: false,
       templates: {
-          header: '' +
-            '<div class="row">' +
-            '  {% util.eachComponent(components, function(component) { %}' +
-            '    {% if (!component.hasOwnProperty("tableView") || component.tableView) { %}' +
-            '      <div class="col-sm-2">' +
-            '        <strong>{{ component.label }}</strong>' +
-            '      </div>' +
-            '    {% } %}' +
-            '  {% }) %}' +
-            '</div>',
-          row: '' +
-            '<div class="row">' +
-            '  {% util.eachComponent(components, function(component) { %}' +
-            '    {% if (!component.hasOwnProperty("tableView") || component.tableView) { %}' +
-            '      <div class="col-sm-2">' +
-            '        {{ row[component.key] }}' +
-            '      </div>' +
-            '    {% } %}' +
-            '  {% }) %}' +
-            '  <div class="col-sm-2">' +
-            '    <div class="btn-group pull-right">' +
-            '      <div class="btn btn-default btn-sm editRow"><i class="bi bi-edit"></i></div>' +
-            '      <div class="btn btn-danger btn-sm removeRow"><i class="bi bi-trash"></i></div>' +
-            '    </div>' +
-            '  </div>' +
-            '</div>'
+        header: '' +
+          '<div class="row">' +
+          '  {% util.eachComponent(components, function(component) { %} ' +
+          '  {% if (!component.hasOwnProperty("tableView") || component.tableView) { %}'+
+          '    <div class="col-sm-2">' +
+          '      <strong>{{ component.label }}</strong>' +
+          '    </div>' +
+          '  {% } %}'+
+          '  {% }) %}' +
+          '</div>',
+        row: '' +
+          '<div class="row">' +
+          '  {%util.eachComponent(components, function(component) { %}' +
+          '{% if (!component.hasOwnProperty("tableView") || component.tableView) { %} '+
+          '    <div class="col-sm-2">' +
+          '      {{ row[component.key] }}' +
+          '    </div>' +
+          '  {% } %} ' +
+          '  {% }) %}' +
+          '  <div class="col-sm-2">' +
+          '    <div class="btn-group pull-right">' +
+          '      <div class="btn btn-default btn-sm editRow"><i class="bi bi-edit"></i></div>' +
+          '      <div class="btn btn-danger btn-sm removeRow"><i class="bi bi-trash"></i></div>' +
+          '    </div>' +
+          '  </div>' +
+          '</div>',
         footer: ''
       },
       components: [
