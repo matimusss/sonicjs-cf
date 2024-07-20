4d92576bd6271d1d2bd29f8de4e0c85e65477243
matimusa@gmail.com <ejemplo@email.com>
2024-07-20 07:37:29 -0300

    asdasdç

--- src/cms/data/d1-data.ts
+++ src/cms/data/d1-data.ts
@@ -43,9 +43,9 @@ export async function getD1ByTableAndSlug_view(db, table, id) {
 
 
 export async function getProduct(db, id) {
-    const productQuery = `    SELECT 
-            p.id AS product_id,
-            p.slug,
+    const productQuery = `SELECT 
+    p.id AS product_id,
+    p.slug,
     p.product_name,
     p.sku,
     p.sale_price,
@@ -55,37 +55,68 @@ export async function getProduct(db, id) {
     p.short_description,
     p.product_description,
     p.product_type,
-          json_group_array(
-                DISTINCT  json_object(
-                    'attribute_name', a.attribute_name,
-                    'attribute_value', av.attribute_value
-            
-                    )
-            ) AS variant_attributes,
-           json_group_array(
+    
+    -- Atributos generales del producto
+    json_group_array(
+        DISTINCT json_object(
+            'attribute_name', a.attribute_name,
+            'attribute_value', av.attribute_value
+        )
+    ) AS product_attributes,
+    
+    -- Detalles de variantes
+    json_group_array(
+        DISTINCT json_object(
+            'variant_option', v.variant_option,
+            'variant_title', vo.title,
+            'variant_image_id', vo.image_id,
+            'variant_sale_price', vo.sale_price,
+            'variant_compare_price', vo.compare_price,
+            'variant_buying_price', vo.buying_price,
+            'variant_quantity', vo.quantity,
+            'variant_active', vo.active,
+            json_group_array(
                 DISTINCT json_object(
-                    'tag_name', t.tag_name,
-                    'tag_icon', t.icon
-                    )
-            ) AS tags
+                    'variant_attribute_name', av_attr.attribute_name,
+                    'variant_attribute_value', avv.attribute_value
+                )
+            ) AS variant_attributes
+        )
+    ) AS variant_details,
+    
+    -- Etiquetas del producto
+    json_group_array(
+        DISTINCT json_object(
+            'tag_name', t.tag_name,
+            'tag_icon', t.icon
+        )
+    ) AS tags
 
-       
-            FROM products p
-       
-            LEFT JOIN product_attributes pa ON p.id = pa.product_id
-        LEFT JOIN attributes a ON pa.attribute_id = a.id
-        LEFT JOIN product_attribute_values pav ON pa.id = pav.product_attribute_id
-        LEFT JOIN attribute_values av ON pav.attribute_value_id = av.id
-       
+FROM products p
 
-        LEFT JOIN product_tags pt ON p.id = pt.product_id
-        LEFT JOIN tags t ON pt.tag_id = t.id
-        
-        WHERE p.id = ?
-        GROUP BY p.id;`;
+-- Uniones para variantes
+LEFT JOIN variants v ON p.id = v.product_id
+LEFT JOIN variant_options vo ON v.id = vo.variant_id
+LEFT JOIN variant_values vv ON v.id = vv.variant_id
+LEFT JOIN product_attribute_values pavv ON vv.product_attribute_value_id = pavv.id
+LEFT JOIN attribute_values avv ON pavv.attribute_value_id = avv.id
+LEFT JOIN attributes av_attr ON pavv.product_attribute_id = av_attr.id
 
+-- Uniones para atributos generales del producto
+LEFT JOIN product_attributes pa ON p.id = pa.product_id
+LEFT JOIN attributes a ON pa.attribute_id = a.id
+LEFT JOIN product_attribute_values pav ON pa.id = pav.product_attribute_id
+LEFT JOIN attribute_values av ON pav.attribute_value_id = av.id
 
+-- Uniones para etiquetas
+LEFT JOIN product_tags pt ON p.id = pt.product_id
+LEFT JOIN tags t ON pt.tag_id = t.id
 
+-- Filtro y agrupación
+WHERE p.id = ?
+GROUP BY p.id;
+    
+        `;
 
   
   const productQuery222 = `SELECT
@@ -160,6 +191,7 @@ LEFT JOIN product_coupons pco ON p.id = pco.product_id
 LEFT JOIN coupons co ON pco.coupon_id = co.id
 LEFT JOIN product_tags pt ON p.id = pt.product_id
 LEFT JOIN tags t ON pt.tag_id = t.id
+
 LEFT JOIN variants v ON p.id = v.product_id
 LEFT JOIN variant_options vo ON p.id = vo.product_id  
 LEFT JOIN variant_values vv ON v.id = vv.variant_id
