a52e5a5bbacee08805f313a46af6bcdfe88f2f86
matimusa@gmail.com <ejemplo@email.com>
2024-07-25 15:22:46 -0300

    asd

--- public/js/form-products.js
+++ public/js/form-products.js
@@ -132,6 +132,22 @@ function createAttributesForm(configData, productData) {
         input: true,
         templates: {
           header: '' +
+         ' <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
+          ' <div class="modal-dialog">' +
+            ' <div class="modal-content">' +
+              ' <div class="modal-header">' +
+              '   <h5 class="modal-title" id="exampleModalLabel">Form.io Form</h5>' +
+               '  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
+               '</div>' +
+               '<div class="modal-body">' +
+           'asdasdasdasdasd' +
+               '</div>' +
+               '<div class="modal-footer">' +
+                 '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>' +
+               '</div>' +
+             '</div>'+
+           '</div>'+
+         '</div>'+
             '<div class="row">' +
             '  {% util.eachComponent(components, function(component) { %} ' +
             '    <div class="col-sm-2">' +
--- src/cms/admin/theme.tsx
+++ src/cms/admin/theme.tsx
@@ -234,22 +234,6 @@ export const Layout: FC<{
     <html lang='en' data-bs-theme='auto'>
       <Head />
       <body>
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Form.io Form</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-    asdasdasdasdasd
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
         <ToggleTheme />
         <header class='navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow'>
           <a
