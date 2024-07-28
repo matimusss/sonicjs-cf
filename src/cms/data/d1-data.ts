76ade853a33432757c84496670d3769c7de31c5d
matimusa@gmail.com <ejemplo@email.com>
2024-07-28 13:36:53 -0300

    asd

--- src/cms/data/d1-data.ts
+++ src/cms/data/d1-data.ts
@@ -52,7 +52,7 @@ SELECT
         DISTINCT json_object(
             'attribute_id', a.id,
             'attribute_name', a.attribute_name,
-            'attribute_id', av.id,
+            'attribute_value_id', av.id,
             'attribute_value', av.attribute_value
         )
     ) AS product_attributes,
