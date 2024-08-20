d505aa5d1d968ea290f63e1d7604a12b31063565
matimusa@gmail.com <ejemplo@email.com>
2024-08-19 22:54:26 -0300

    asd

--- public/js/uppbkp.js
+++ public/js/uppbkp.js
@@ -1,8 +1,18 @@
 import {
   Uppy,
   Dashboard,
-  XHRUpload
+  Webcam,
+  XHRUpload,
+  DragDrop,
+  GoogleDrive,
+  Dropbox,
+  Instagram,
+  Facebook,
+  ScreenCapture
 } from 'https://releases.transloadit.com/uppy/v4.1.1/uppy.min.mjs';
+
+
+
 
 async function generateSignature() {
   try {
@@ -33,11 +43,26 @@ async function generateSignature() {
   }
 }
 
-const cloudName =  "dmyt0fswa";
 
 // Inicialización de Uppy con varios plugins
 const uppy = new Uppy({ debug: true, autoProceed: false })
   .use(Dashboard, { target: '#uppyDashboard', inline: true })
+  .use(Transloadit, {
+	assemblyOptions: {
+			params: {
+				auth: { key: '3f1f8718e27ab626f84dcbe1669c9ea9' }, //KEY DE TRANLSOADIT
+				template_id: '0df3da82d5cb4d19bec255a8896b3ec4', //TEMPLATE ID DE TRANSLOADIT
+			},
+		},
+	})
+ .use(Instagram, {
+    companionUrl: COMPANION_URL, //IMPORTADO DE TRANSLOADIT PLUGIN 
+    companionAllowedHosts: COMPANION_ALLOWED_HOSTS, //IMPORTADO DE TRANLSOADIT PLUGIN
+    companionKeysParams: {
+      key: '3f1f8718e27ab626f84dcbe1669c9ea9', //KEY DE TRANSLOADIT
+      credentialsName: 'testmatimusss', //NOBMRE DADO AL CREDENTIAL TEMPLATE EN TRANSLOADIT
+    },
+  })
   .use(XHRUpload, {
     endpoint: `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`,
     fieldName: 'file',
@@ -48,7 +73,8 @@ const uppy = new Uppy({ debug: true, autoProceed: false })
           method: 'POST',
           url: `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`,
           fields: {
-            api_key: "152897242549548",
+            upload_preset: uploadPreset,
+            api_key: apiKey,
             timestamp: signatureData.timestamp,
             signature: signatureData.signature,
           },
@@ -61,6 +87,7 @@ const uppy = new Uppy({ debug: true, autoProceed: false })
 uppy.on('upload-progress', (file, progress) => {
   console.log(`Uploading ${file.name}: ${progress.bytesUploaded} / ${progress.bytesTotal}`);
 });
+
  
 uppy.on('upload-success', (file, response) => {
   console.log(`File ${file.name} uploaded successfully`);
