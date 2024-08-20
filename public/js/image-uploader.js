88996882a1b843ada6b5db70eace936b15152c33
matimusa@gmail.com <ejemplo@email.com>
2024-08-20 19:52:13 -0300

    asd

--- public/js/image-uploader.js
+++ public/js/image-uploader.js
@@ -376,7 +376,7 @@ let preloaded = [
   {id: 3, src: 'https://picsum.photos/500/500?random=3'},
   {id: 4, src: 'https://picsum.photos/500/500?random=4'},
   {id: 5, src: 'https://picsum.photos/500/500?random=5'},
-  {id: 6, src: 'https://picsum.photos/500/500?random=6'},
+  {id: "id_de_prueba", src: 'https://picsum.photos/500/500?random=6'},
 ];
 
 $('.input-images-2').imageUploader({
