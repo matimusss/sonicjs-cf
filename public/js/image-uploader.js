153896767039b6210d859207ff88799385c8a358
matimusa@gmail.com <ejemplo@email.com>
2024-08-20 19:13:29 -0300

    asd

--- public/js/image-uploader.js
+++ public/js/image-uploader.js
@@ -363,28 +363,39 @@
 }(jQuery));
 
 
-let preloaded = [
-  {id: 1, src: 'https://picsum.photos/500/500?random=1'},
-  {id: 2, src: 'https://picsum.photos/500/500?random=2'},
-  {id: 3, src: 'https://picsum.photos/500/500?random=3'},
-  {id: 4, src: 'https://picsum.photos/500/500?random=4'},
-  {id: 5, src: 'https://picsum.photos/500/500?random=5'},
-  {id: 6, src: 'https://picsum.photos/500/500?random=6'},
-];
 
-$('.input-images-2').imageUploader({
-  preloaded: preloaded,
-  imagesInputName: 'photos',
-  preloadedInputName: 'old',
-  maxSize: 2 * 1024 * 1024,
-  maxFiles: 10
-});
+// La función que deseas ejecutar después del timeout
+function myFunction() {
 
-$('.input-images-2').imageUploader({
-  preloaded: preloaded,
-  imagesInputName: 'photos',
-  preloadedInputName: 'old',
-  maxSize: 2 * 1024 * 1024,
-  maxFiles: 10
-});
 
+  let preloaded = [
+    {id: 1, src: 'https://picsum.photos/500/500?random=1'},
+    {id: 2, src: 'https://picsum.photos/500/500?random=2'},
+    {id: 3, src: 'https://picsum.photos/500/500?random=3'},
+    {id: 4, src: 'https://picsum.photos/500/500?random=4'},
+    {id: 5, src: 'https://picsum.photos/500/500?random=5'},
+    {id: 6, src: 'https://picsum.photos/500/500?random=6'},
+  ];
+  
+  $('.input-images-2').imageUploader({
+    preloaded: preloaded,
+    imagesInputName: 'photos',
+    preloadedInputName: 'old',
+    maxSize: 2 * 1024 * 1024,
+    maxFiles: 10
+  });
+  
+  $('.input-images-2').imageUploader({
+    preloaded: preloaded,
+    imagesInputName: 'photos',
+    preloadedInputName: 'old',
+    maxSize: 2 * 1024 * 1024,
+    maxFiles: 10
+  });
+  
+  
+  console.log("La función se ha ejecutado después de 1 segundo");
+}
+
+// Configura el timeout para que ejecute la función después de 1 segundo
+setTimeout(myFunction, 1000);
